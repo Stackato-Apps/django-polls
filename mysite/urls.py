41b1644b7c97e982ec1587fb69e93a7812f81b38
Billy Tat <billyt@activestate.com>
2014-05-01 14:49:17 -0700

    Fix NoReverseMatch at / error

--- mysite/urls.py
+++ mysite/urls.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^$', include('polls.urls', namespace="polls")),
     url(r'^polls/', include('polls.urls', namespace="polls")),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', include('polls.urls', namespace="polls")),
 )
